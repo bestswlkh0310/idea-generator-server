b22d61c4a750fe44486e918ab8ae7193f2b0fae2
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-02-08 14:05:48 +0900

    feat : create generate idea api

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 import express from 'express';
+import { generateIdea } from './openai/ai.js';
 
 const app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.post('/api/v1/generate', async (req, res) => {
+    const { prompt, count } = req.body;
+    if (prompt === undefined || count === undefined) {
+        res.status(400).send({
+            message: '입력값이 올바르지 않습니다.'
+        });
+        return;
+    }
+    try {
+        const result = await generateIdea(prompt, count);
+        res.send(result);
+    } catch (error) {
+        console.log(error)
+        res.status(204).send({
+            message: '오류가 발생했습니다. 다시 시도해주세요.',
+        });
+    }
+});
+
+const port = 3030;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
